f43d42b5b3114ad5864325ddb5bd185735fdc038
aninoh <a.ninoh@uniandes.edu.co>
2026-02-06 22:35:35 -0500

    cambios array

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -22,4 +22,18 @@ def is_present(my_list, element, cmp_function):
                  if keyexist:
                                 return keypos
          return -1
+def add_first(my_list, element):
+        my_list['elements'].insert(0, element)
+        my_list['size'] += 1
+        return my_list
+def add_last(my_list, element):
+        my_list['elements'].append(element)
+        my_list['size'] += 1
+        return my_list
+def size(my_list):
+        return my_list['size']
+def first_element(my_list):
+    if my_list["size"] == 0:
+        return None
+    return my_list["elements"][0] 
                          
